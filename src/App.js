91c4b88df8a8f49e6ddcf0959c3df89a488d9dce
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-06-11 18:02:20 +0100

    - add react popper

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <DownshiftOne />
+        {/* <DownshiftOne />
         <DownshiftTwo />
         <DownshiftThree />
-        <DownshiftFour />
+        <DownshiftFour /> */}
       </div>
     );
   }
